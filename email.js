611d720c962ea40bc9c7cfad8b7750ef04072ebb
Sunil Kumar C <sunilkumar.c682@gmail.com>
2017-06-27 20:13:51 +0530

    Sending a tempalte html file working
    File tempalte.html has been added. The code is now able to send
    the template in the email body.

--- email.js
+++ email.js
@@ -2,17 +2,20 @@ var api_key = 'key-8dc58954dbf80c9bc284e55ef3adf842';
 var domain = 'sandbox654938629c71400ca29474cb5b35b233.mailgun.org';
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 var mailcomposer = require('mailcomposer')
+var fs = require('fs');
 
 function Mail() {
     function sendMail(from, to, subject, text) {
-        var contents = "<!DOCTYPE><html>" + text + "<br/><br/>Access your birthday card <a href=\"http://pump-attendant-yards-47683.bitballoon.com/\">here</a></html>";
+        // var contents = "<!DOCTYPE><html>" + text + "<br/><br/>Access your birthday card <a href=\"http://pump-attendant-yards-47683.bitballoon.com/\">here</a></html>";
+        var contents = fs.readFileSync('./template.html', 'utf8');
+        console.log(contents);
         var mail = mailcomposer({
             from: from,
             to: to,
             subject: subject,
             text: text,
             html: contents
-            ,inline: './images/gift_box.png' // Not working
+            // ,inline: './images/gift_box.png' // Not working
         });
 
         mail.build(function (mailBuildError, message) {
